8707b1df5c5fcecc77d6be61b84c027af122d71f
HubertNastaly <h.nastaly@gmail.com>
2023-05-15 13:31:48 +0200

    Display pizza menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import * as Tabs from '@radix-ui/react-tabs';
-import { StepTabs } from './steps';
+import { StepTabs } from './tabs';
 import { PageContent } from './components';
+import { PizzaStep } from './tabs/steps';
+import { steps } from './tabs/utils';
 
 export const App = () => {
   return (
     <PageContent>
-      <Tabs.Root>
+      <Tabs.Root defaultValue={steps[0]}>
         <StepTabs />
+        <PizzaStep />
       </Tabs.Root>
     </PageContent>
   )
---
+++ src/tabs/steps/StepContent.tsx
@@ -0,0 +1,7 @@
+import * as Tabs from '@radix-ui/react-tabs';
+import { styled } from "@stitches/react";
+
+export const StepContent = styled(Tabs.Content, {
+  width: '100%',
+  padding: '32px 0'
+})
